c91d2833f93b748f68c328fa1562b05eb81cc952
skearya <77034153+skearya@users.noreply.github.com>
2024-01-18 23:17:56 +0000

    d3 p1!

--- day_3/src/bin/part_one.rs
+++ day_3/src/bin/part_one.rs
@@ -1,42 +1,50 @@
 fn main() {
-    let lines = include_str!("../input.txt").lines().collect::<Vec<_>>();
+    let lines: Vec<&str> = include_str!("../input.txt").lines().collect();
+    let mut numbers: Vec<u32> = Vec::new();
 
-    if let Some(line) = lines.first() {
-        let line_index = 0;
-        let numbers: Vec<_> = line.char_indices().filter(|x| x.1 != '.').collect();
+    for (line_idx, line) in lines.iter().enumerate() {
+        let mut look_after = 0;
 
-        let mut look_past = 0;
+        for (char_idx, char) in line.char_indices() {
+            if char.is_numeric() && char_idx >= look_after {
+                let mut number = String::new();
 
-        for (index, char) in &numbers {
-            if char.is_numeric() && !(index <= &look_past) {
-                let mut number: Vec<u32> = Vec::new();
-                number.push(char.to_digit(10).unwrap());
-
-                let mut index2 = index + 1;
-
-                while let Some(element) = numbers.iter().find(|x| x.0 == index2) {
-                    number.push(element.1.to_digit(10).unwrap());
-                    index2 += 1;
+                look_after = char_idx;
+                while let Some(other_char) = line.chars().nth(look_after) {
+                    if other_char.is_numeric() {
+                        number.push(other_char);
+                        look_after += 1;
+                    } else {
+                        break;
+                    }
                 }
 
-                look_past = index + number.len() - 1;
+                let l_r_chars = [
+                    char_idx
+                        .checked_sub(1)
+                        .and_then(|idx| line.chars().nth(idx)),
+                    line.chars().nth(look_after),
+                ];
 
-                let (prev_char, next_char) =
-                    (line.chars().nth(index - 1), line.chars().nth(look_past));
+                let adjacent_lines = [
+                    line_idx.checked_sub(1).and_then(|idx| lines.get(idx)),
+                    lines.get(line_idx + 1),
+                ];
 
-                let condition = |x: char| x != '.' && !x.is_numeric();
+                let indices = char_idx.saturating_sub(1)..look_after + 1;
+                let adjacent_chars = adjacent_lines
+                    .iter()
+                    .flatten()
+                    .flat_map(|line| indices.clone().flat_map(|i| line.chars().nth(i)));
 
-                if prev_char.is_some_and(condition) || next_char.is_some_and(condition) {
-                    println!(
-                        "{}: {}, {}",
-                        char,
-                        prev_char.unwrap_or('X'),
-                        next_char.unwrap_or('X')
-                    )
+                for char in adjacent_chars.chain(l_r_chars.into_iter().flatten()) {
+                    if !char.is_numeric() && char != '.' {
+                        numbers.push(number.parse().unwrap());
+                    }
                 }
-
-                let (prev_line, next_line) = (lines.get(line_index - 1), lines.get(line_index + 1));
             }
         }
     }
+
+    println!("sum: {:?}", numbers.iter().sum::<u32>());
 }
