9eb85fb48bed9b1d6462b333d1f0090f44e991be
skearya <77034153+skearya@users.noreply.github.com>
2024-05-26 02:29:24 -0400

    d10 p1

---
+++ 2023/day_10/rust/src/bin/part_one.rs
@@ -0,0 +1,122 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Direction {
+    North,
+    South,
+    West,
+    East,
+}
+
+impl Direction {
+    fn apply_to_coords(&self, (row, col): (usize, usize)) -> (usize, usize) {
+        match self {
+            Self::North => (row - 1, col),
+            Self::South => (row + 1, col),
+            Self::West => (row, col - 1),
+            Self::East => (row, col + 1),
+        }
+    }
+}
+
+impl std::ops::Not for Direction {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        match self {
+            Self::North => Self::South,
+            Self::South => Self::North,
+            Self::West => Self::East,
+            Self::East => Self::West,
+        }
+    }
+}
+
+fn main() {
+    let points: Vec<Vec<char>> = include_str!("../../../input.txt")
+        .lines()
+        .enumerate()
+        .map(|line| line.1.char_indices().map(|c| (c.1)).collect())
+        .collect();
+
+    let starting_coords = points
+        .iter()
+        .enumerate()
+        .find_map(|(row, chars)| chars.iter().position(|c| *c == 'S').map(|col| (row, col)))
+        .unwrap();
+
+    let starting_dir = starting_dir(&points, &starting_coords);
+
+    let path = traverse(
+        &points,
+        &starting_dir.apply_to_coords(starting_coords),
+        starting_dir,
+    );
+
+    println!("steps to max point: {}", path.len() / 2);
+}
+
+fn tile_to_possible_dirs(tile: char) -> Option<(Direction, Direction)> {
+    match tile {
+        '|' => Some((Direction::North, Direction::South)),
+        '-' => Some((Direction::East, Direction::West)),
+        'L' => Some((Direction::North, Direction::East)),
+        'J' => Some((Direction::North, Direction::West)),
+        '7' => Some((Direction::South, Direction::West)),
+        'F' => Some((Direction::South, Direction::East)),
+        _ => None,
+    }
+}
+
+fn exclude(dirs: &(Direction, Direction), dir: Direction) -> Option<Direction> {
+    if dirs.0 == dir {
+        Some(dirs.1)
+    } else if dirs.1 == dir {
+        Some(dirs.0)
+    } else {
+        None
+    }
+}
+
+fn starting_dir(points: &Vec<Vec<char>>, (row, col): &(usize, usize)) -> Direction {
+    let get = |row: usize, col: usize, dir: Direction| {
+        points
+            .get(row)
+            .and_then(|row| row.get(col))
+            .and_then(|tile| tile_to_possible_dirs(*tile))
+            .and_then(|paths| exclude(&paths, dir))
+    };
+
+    let north = row
+        .checked_sub(1)
+        .and_then(|row| get(row, *col, !Direction::North));
+    let south = get(row + 1, *col, !Direction::South);
+    let west = col
+        .checked_sub(1)
+        .and_then(|col| get(*row, col, !Direction::West));
+    let east = get(*row, col + 1, !Direction::East);
+
+    north.or(south).or(west).or(east).unwrap()
+}
+
+fn traverse(
+    points: &Vec<Vec<char>>,
+    (mut row, mut col): &(usize, usize),
+    mut prev_dir: Direction,
+) -> Vec<(usize, usize)> {
+    let mut path = Vec::from([(row, col)]);
+
+    loop {
+        path.push((row, col));
+
+        let tile = points[row][col];
+
+        if tile == 'S' {
+            break;
+        }
+
+        let possible_dirs = tile_to_possible_dirs(tile).unwrap();
+        prev_dir = exclude(&possible_dirs, !prev_dir).unwrap();
+        (row, col) = prev_dir.apply_to_coords((row, col));
+    }
+
+    path
+}
