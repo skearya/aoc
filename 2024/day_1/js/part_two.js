f6e16ba3fac68c4fe905e3db931dfb1481060853
skearya <x@example.com>
2024-12-01 01:00:18 -0500

    2024 d1 p1

---
+++ 2024/day_1/js/part_two.js
@@ -0,0 +1,21 @@
+import { readFileSync } from "node:fs";
+
+const input = readFileSync("input.txt", "utf-8");
+
+let nums = input.split("\n").map((x) =>
+  x
+    .split(" ")
+    .filter((x) => x !== "")
+    .map((x) => parseInt(x, 10))
+);
+
+let left = nums.map((x) => x[0]).sort((a, b) => a - b);
+let right = nums.map((x) => x[1]).sort((a, b) => a - b);
+
+let sum = 0;
+
+for (let i = 0; i < left.length; i++) {
+  sum += left[i] * right.filter((x) => x === left[i]).length;
+}
+
+console.log(sum);
